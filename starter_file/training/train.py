086121b48b1632edebeb242fa7a017390586ff43
Yallamanda, Daghupati R <daghupati.r.yallamanda@intel.com>
2021-03-23 21:40:58 -0700

    updated train.py

--- starter_file/training/train.py
+++ starter_file/training/train.py
@@ -3,63 +3,34 @@ import argparse
 import os
 import numpy as np
 from sklearn.metrics import mean_squared_error
-from sklearn.metrics import roc_auc_score, roc_curve
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
-from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Workspace, Dataset
 
-def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
-    # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    return x_df, y_df
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = TabularDatasetFactory.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=12345)
 
 run = Run.get_context()
-    
+ws = run.experiment.workspace
+
 
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--data', type=str, help="Loading dataset")
     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
 
     args = parser.parse_args()
+    
+    # split data to train and test sets
+    dataset = Dataset.get_by_name(ws, name='diabetes_data_set')
+    dataset = dataset.to_pandas_dataframe()
+    x = dataset.drop(columns=['Outcome'])
+    y = dataset['Outcome']
+    x_train, x_test, y_train, y_test = train_test_split(x,y)
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
@@ -67,14 +38,12 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
-
-    y_scores = model.predict_proba(x_test)
-    auc = roc_auc_score(y_test,y_scores[:,1])    
-    run.log('AUC', np.float(auc))
-
+    
     os.makedirs('outputs', exist_ok=True)
-    joblib.dump(value=model, filename='outputs/bankmarketing_model.pkl')
+    
+    joblib.dump(model, 'outputs/model.joblib')
+    
+    run.log("Accuracy", np.float(accuracy))
 
 if __name__ == '__main__':
     main()
